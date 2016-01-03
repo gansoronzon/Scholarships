9c749f120bbbe2dce5ffcbbc60e89e122b13ebc5
Gansoronzon <gansoronzon.tsee@gmail.com>
2016-01-03 16:28:43 -0600

    Initial Commit

---
+++ Scholarships 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scholarships 2.0
+//
+//  Created by Gansoronzon on 1/3/16.
+//  Copyright © 2016 Gansoronzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
